09030e5e1176d0fb9fb4fb7cd31ddd91b5995f6d
liqiang0335@qq.com <mm020318@2>
2018-08-02 14:27:41 +0800

    add dynamic import

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
-const registeVueCompsAst = require("./src/registeVueComps");
+const babel = require("babel-core");
+const path = require("path");
+const fs = require("fs");
+const getSource = name => path.join(__dirname, `./src/${name}.js`);
+const compsSource = fs.readFileSync(getSource("comps"), "utf-8");
+const dynamicSource = fs.readFileSync(getSource("dynamic"), "utf-8");
+const defAst = babel.transform(compsSource).ast;
+
+const transform = function(source) {
+  return babel.transform(source).ast;
+};
 
 /**
  * handler
  */
+const cache = {};
 const handler = {
-  ynRegisteVueComps(path) {
-    path.replaceWithMultiple(registeVueCompsAst);
+  importVueComps(path) {
+    const { node } = path;
+    if (node.arguments.length < 1) {
+      path.replaceWithMultiple(defAst);
+    } else {
+      const folderName = node.arguments[0].value;
+      const comps = node.arguments[1].elements.map(item => item.value);
+      const regName = comps.join("|");
+      if (cache[regName]) {
+        path.replaceWithMultiple(cache[regName]);
+        return;
+      }
+      const source = dynamicSource
+        .replace("@folderName", folderName)
+        .replace("@regName", regName);
+      const ast = transform(source);
+      cache[regName] = ast;
+      path.replaceWithMultiple(ast);
+    }
   }
 };
 
-module.exports = function(babel) {
+module.exports = function() {
   return {
     visitor: {
       CallExpression(path) {
-        const { node } = path;
-        const name = node.callee.name;
-        const params = node.arguments.map(item => item.value);
-        handler[name] && handler[name](path, params);
+        const name = path.node.callee.name;
+        if (handler[name]) {
+          handler[name](path);
+        }
       }
     }
   };
